c054c7401d98fc994411c7b7db8f0648aba1c761
Brandon Yuong <brandonyuong@gmail.com>
2020-10-06 05:44:28 -0700

    Initial data cleaning and analysis.

--- main.py
+++ main.py
@@ -4,21 +4,80 @@ import pandas as pd
 from time import clock
 
 
-def data_cleaning():
+def initial_data_cleaning():
     user_interactions_df = pd.read_csv('users_interactions.csv')
     shared_articles_df = pd.read_csv('shared_articles.csv')
+    print(shared_articles_df)
 
-    # scrub out junk rows
-    shared_articles_df = shared_articles_df[shared_articles_df.eventType != 'CONTENT']
+    # group event types
+    user_interactions_df['COUNTER'] = 1  # initial count = 1
+    user_int_event_count = user_interactions_df.groupby(['contentId', 'eventType'])[
+        'COUNTER'].sum().reset_index()  # sum function
+    print("== Grouped Events ==")
+    print(user_int_event_count)
 
-    # Create left join of 'user_int' and 'shared_art' dfs
-    virality_df = pd.merge(user_interactions_df, shared_articles_df, on='contentId',
-                           how='left')
+    events_df = user_int_event_count.pivot_table('COUNTER', ['contentId'], 'eventType')
+    events_df = events_df.fillna(0)
+    print("== events_df ==")
+    print(events_df)
+
+    events_df['Virality'] = (events_df['VIEW'] + 4 * events_df['LIKE'] + 10 * events_df[
+        'COMMENT CREATED'] + 25 * events_df['FOLLOW'] + 100 * events_df['BOOKMARK'])
+
+    # Create left join of 'events' and 'shared_art' dfs
+    virality_df = pd.merge(events_df, shared_articles_df, on='contentId', how='left')
+
+    # Remove columns with missing/NaN/null values
+    num_cols = get_cols_w_no_nans(virality_df)
+    cat_cols = get_cols_w_no_nans(virality_df, 'no_num')
+
+    print('Number of numerical columns with no nan values :', len(num_cols))
+    print('Number of nun-numerical columns with no nan values :', len(cat_cols))
+
+    cols = get_cols_w_no_nans(virality_df)
+    print(cols)
+    virality_df.hist(figsize=(12, 10))
+    plt.savefig('virality_hist.png')
+
+    virality_df = virality_df[cols]
+
+    # Add word count of 'text' column and drop it
+    virality_df['textWordCount'] = virality_df['text'].str.split().str.len()
+    virality_df = virality_df.drop('text', 1)
+    print(virality_df)
+
+    # Export for future loading
     virality_df.to_csv('virality.csv')
 
 
+def get_cols_w_no_nans(df, col_type='all'):
+    """
+    :param df: The dataframe to process
+    :param col_type:
+        num : to only get numerical columns with no nans
+        no_num : to only get non-numerical columns with no nans
+        all : to get any columns with no nans
+    :return:
+    """
+    if col_type == 'num':
+        predictors = df.select_dtypes(exclude=['object'])
+    elif col_type == 'no_num':
+        predictors = df.select_dtypes(include=['object'])
+    else:
+        predictors = df
+
+    cols_with_no_nans = []
+    for col in predictors.columns:
+        if not df[col].isnull().any():
+            cols_with_no_nans.append(col)
+    return cols_with_no_nans
+
+
 def main():
-    virality_df = pd.read_csv('virality.csv')
+    # Data Cleaning: run the first time
+    initial_data_cleaning()
+
+    #virality_df = pd.read_csv('virality.csv')
 
 
 if __name__ == '__main__':
